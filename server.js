75161894f32ffd483214bbf70e3ffca124bdfd2e
wonder95 <killshot91@gmail.com>
2016-03-30 13:30:28 -0700

    Adding node_modules/ to .gitignore.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').load();
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var twit = require('twit');
+
+var Twit = new twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+var stream = Twit.stream('statuses/filter', { track: tcot });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +20,7 @@ app.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, function() {
+server.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 3000');
 });
 
@@ -21,4 +31,13 @@ io.on('connect', function(client) {
     console.log(data);
     client.emit('messages', 'Hello from the server.');
   });
+});
+
+io.sockets.on("connection", function(socket) {
+  console.log ("Client connected");
+  
+  stream.on
+
+
+
 });
