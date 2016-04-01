e306ec77d350e9d382d18ca0b00d50a65fc2e150
Eric Lawrence <eric@squaretone.com>
2016-03-31 23:55:35 -0500

    Make checking for connection an async function

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-require("dotenv").load();
+require("dotenv").config();
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var Twitter = require('twitter');
 
-var isClientConnected = false;
+var port = process.env.PORT || 3000;
 
 var client = new Twitter({
   consumer_key: process.env.API_KEY,
@@ -13,6 +14,7 @@ var client = new Twitter({
   access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
+console.log('process.env.API_KEY', process.env.API_KEY);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
@@ -21,16 +23,19 @@ app.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on port 3000');
-});
+/**
+ * Make call to client to determine if Twitter auth was successful
+ * @TODO: Check to see if there is a specific method to do this
+ * @param  {Function} cb Callback function
+ */
+var checkIfConnected = function(cb) {
+  client.get('favorites/list', function(error, tweets, response){
+    cb (!error);
+  });
+};
 
-// Make a call to determine if Twiiter authentication worked
-// @TODO: Check to see if there is a specific method to do this
-client.get('favorites/list', function(error, tweets, response){
-  if (!error) {
-    isClientConnected = true;
-  }
+server.listen(port, function() {
+  console.log('Server running at http://localhost:%s', port);
 });
 
 io.on('connection', function(socket) {
@@ -41,7 +46,9 @@ io.on('connection', function(socket) {
     socket.emit('messages', 'A client has joined.');
 
     // Send the status of the client
-    socket.emit('status', isClientConnected);
+    checkIfConnected(function(isClientConnected) {
+      socket.emit('status', isClientConnected);
+    });
   });
 
   var stream = client.stream('statuses/filter', { track: 'tcot' }, function(stream) {
